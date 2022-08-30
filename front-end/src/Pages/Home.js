e76c9f4abfe7e2e7e9428ed83c9265daa883317b
Samantha Figueroa <96448061+samfigueroa92@users.noreply.github.com>
2022-08-29 22:06:52 -0400

    adds more styling

--- front-end/src/Pages/Home.js
+++ front-end/src/Pages/Home.js
@@ -1,7 +1,29 @@
+// import { useState } from "react";
+import axios from "axios";
+
 const Home = () => {
+    // const [request, setRequest] = useState();
+    // const [response, setResponse] = useState();
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        try{
+            axios.get(`https://api.rawg.io/api/games?key=${process.env.REACT_APP_GAME_API_KEY}`)
+            .then(res => console.log(res))
+            .catch(err => console.error(err))
+        }catch(err){
+            return err;
+        };
+    };
+
+
     return (
         <div className="home">
             <h1>Welcome to GameTracker!</h1>
+            <form onSubmit={handleSubmit}>
+                {/* <input type="text" placeholder="Search" id="search-bar" name="search" value={request} onChange={(e) => setRequest(e.target.value)}/> */}
+                <input type="submit" value="Get Games" />
+            </form>
         </div>
     );
 };
