32122f6563814ab382e29f642ef4a4b2d72ce373
Samantha Figueroa <96448061+samfigueroa92@users.noreply.github.com>
2022-09-01 20:13:48 -0400

    adds stats function

--- front-end/src/Components/Games.js
+++ front-end/src/Components/Games.js
@@ -9,26 +9,36 @@ const Games = ({ games }) => {
   const gamesCopied = [...games];
 
   const getStats = () => {
-    const counts = {};
     const progressArr = games.map(game => game.progress);
-    let maxNum = progressArr.length;
+    const counts = { maxNum: progressArr.length };
+   
     for(let progress of progressArr){
+        if(progress === "Currently Playing"){
+            progress = "CurrentlyPlaying"
+        }else if(progress === "In Progress"){
+            progress = "InProgress"
+        };
         counts[progress] = counts[progress] ?  counts[progress] + 1 : 1;
     };
 
+    return counts;
+  };
 
-
-    return maxNum;
-  }
-
-  console.log(getStats())
+  const gameStats = getStats();
+  const completedStat = Math.floor(gameStats.Completed / gameStats.maxNum * 100);
+  const playingStat = Math.floor(gameStats.CurrentlyPlaying / gameStats.maxNum * 100);
+  const inProgressStat = Math.floor(gameStats.InProgress/ gameStats.maxNum * 100);
+  const backloggedStat = Math.floor(gameStats.Backlogged / gameStats.maxNum * 100);
+  const abandonedStat = Math.floor(gameStats.Abandoned / gameStats.maxNum * 100);
 
   return (
     <div className="games">
       <section className="stats">
-        <div>stats 1</div>
-        <div>stats 2</div>
-        <div>stats 3</div>
+        <div>Games Completed: {completedStat}%</div>
+        <div>Games Currently Playing: {playingStat}%</div>
+        <div>Games In Progress: {inProgressStat}%</div>
+        <div>Games Backlogged: {backloggedStat}%</div>
+        <div>Games Abandoned: {abandonedStat}%</div>
       </section>
       <Button variant="dark" onClick={() => setSort(!sort)}>
         Sort Alphabetically
