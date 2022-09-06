ba94b9529f517d769ce05e34055b63f6587344db
Samantha Figueroa <96448061+samfigueroa92@users.noreply.github.com>
2022-09-06 19:54:31 -0400

    adds files for deployment

---
+++ back-end/db/prod_schema.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS games_dev;
+
+CREATE TABLE games (
+    id SERIAL PRIMARY KEY,
+    name TEXT NOT NULL,
+    console TEXT,
+    progress TEXT,
+    description TEXT,
+    image TEXT,
+    rating INTEGER,
+    CHECK (rating >= 0 AND rating <= 5),
+    is_favorite BOOLEAN
+);
